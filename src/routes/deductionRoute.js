4637f7190b82e5aac8ea580de169f19973047161
Itumeleng Tsoela <thiotsoela@gmail.com>
2022-03-12 21:57:08 +0200

    added file upload

--- src/routes/deductionRoute.js
+++ src/routes/deductionRoute.js
@@ -15,11 +15,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
 const authMiddleware_1 = require("../middleware/authMiddleware");
+//import upload from '../middleware/upload';
 const index_1 = __importDefault(require("../config/index"));
 const uuid_1 = require("uuid");
+const multer_1 = __importDefault(require("multer"));
 const storage_1 = require("firebase/storage");
 const helperFunctions_1 = require("../utils/helperFunctions");
 const route = express_1.default.Router();
+const memoryStorage = multer_1.default.memoryStorage();
+const multerUpload = (0, multer_1.default)({
+    storage: memoryStorage
+});
+// @desc    uploading a image with budget id
+// @route   GET /api/v1/deductions/image
+// @access  Private
+route.post('/image/:id', multerUpload.single('featuredImage'), (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const file = req.file;
+    const { id } = req.params;
+    if (file) {
+        const storage = (0, storage_1.getStorage)();
+        const reff = (0, storage_1.ref)(storage, `budgets/${id}/${file.originalname}`);
+        // const img = await fetch(file.buffer);
+        // const bytes = await img.blob();
+        yield (0, storage_1.uploadBytes)(reff, file.buffer);
+    }
+    res.send('hello');
+})));
 // @desc    Fetch deductions by the budget id
 // @route   GET /api/v1/deductions/:id
 // @access  Private
@@ -45,7 +66,7 @@ route.get('/:id', authMiddleware_1.protect, (0, express_async_handler_1.default)
 route.post('/:id', authMiddleware_1.protect, (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { username } = req.user;
-    const { amount, description, image, tags, created_on } = req.body;
+    let { amount, description, image, tags, created_on } = req.body;
     if (!amount)
         throw new Error('Provide an amount deducted');
     const { rows: [record] } = yield index_1.default.query(`select * from "BudgetUser" bu where bu.budget_id = $1 and username = $2`, [id, username]);
@@ -54,7 +75,11 @@ route.post('/:id', authMiddleware_1.protect, (0, express_async_handler_1.default
     const newID = (0, uuid_1.v4)();
     const { rows: [newDeduction] } = yield index_1.default.query(`insert into "Deduction" (amount, description, image, tags, id, budgets_id, created_on)
                                                         values ($1, $2, $3, $4, $5, $6, $7) returning *`, [amount, description, image, tags, newID, id, new Date(created_on).toISOString()]);
-    res.json(newDeduction);
+    if (image) {
+        const storage = (0, storage_1.getStorage)();
+        image = yield (0, storage_1.getDownloadURL)((0, storage_1.ref)(storage, `budgets/${id}/${image}`));
+    }
+    res.json(Object.assign(Object.assign({}, newDeduction), { image }));
 })));
 // @desc    Editing a deduction by budget-id and deduction-id
 // @route   POST /api/v1/deductions/:id
@@ -72,27 +97,6 @@ route.post('/:budgetID/:id', authMiddleware_1.protect, (0, express_async_handler
                                                     WHERE id = $5 returning *`, [amount, description, tags, new Date(created_on).toISOString(), id]);
     res.json(deduction);
 })));
-route.get('/sss/sss/ss', (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const storage = (0, storage_1.getStorage)();
-    const listRef = (0, storage_1.ref)(storage, 'budgets');
-    // Find all the prefixes and items.
-    (0, storage_1.listAll)(listRef)
-        .then((res) => {
-        res.items.forEach((itemRef) => {
-            (0, storage_1.getDownloadURL)((0, storage_1.ref)(storage, `budgets/${itemRef.name}`))
-                .then((url) => {
-                console.log(url);
-            })
-                .catch((error) => {
-                console.log(error);
-            });
-        });
-    })
-        .catch((error) => {
-        console.log(error);
-    });
-    res.send({ yes: 'yes' });
-})));
 // @desc    deleting a deductions by the budget-id and deduction-id
 // @route   GET /api/v1/deductions/:budgetID/:id
 // @access  Private
@@ -102,7 +106,18 @@ route.delete('/:budgetID/:id', authMiddleware_1.protect, (0, express_async_handl
     const { rows: [record] } = yield index_1.default.query(`select * from "BudgetUser" bu where bu.budget_id = $1 and username = $2`, [budgetID, username]);
     if (!record)
         throw new Error('You do not have permission to do that');
+    const { rows: [singleDeduction] } = yield index_1.default.query(`select * from "Deduction" where id = $1`, [id]);
     yield index_1.default.query(`delete from "Deduction" where id = $1`, [id]);
+    if (singleDeduction.image) {
+        const storage = (0, storage_1.getStorage)();
+        const desertRef = (0, storage_1.ref)(storage, `budgets/${budgetID}/${singleDeduction.image}`);
+        try {
+            yield (0, storage_1.deleteObject)(desertRef);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
     res.json({ success: true });
 })));
 exports.default = route;
--- src/routes/deductionRoute.ts
+++ src/routes/deductionRoute.ts
@@ -1,11 +1,37 @@
 import express, {Request, Response, NextFunction} from 'express';
 import asyncHandler from "express-async-handler";
+import path from 'path'
 import { protect } from '../middleware/authMiddleware';
+//import upload from '../middleware/upload';
 import pool from '../config/index';
 import { v4 as uuidv4 } from 'uuid';
-import {getStorage, ref, deleteObject, listAll, getDownloadURL} from 'firebase/storage';
+import multer from 'multer';
+import {getStorage, ref, deleteObject, listAll, getDownloadURL, uploadBytes} from 'firebase/storage';
 import { searchForItem } from '../utils/helperFunctions';
 const route = express.Router();
+
+const memoryStorage = multer.memoryStorage();
+const multerUpload = multer({
+    storage: memoryStorage
+})
+
+// @desc    uploading a image with budget id
+// @route   GET /api/v1/deductions/image
+// @access  Private
+route.post('/image/:id', multerUpload.single('featuredImage'), asyncHandler(async (req: Request, res:Response, next: NextFunction) => {
+    const file = req.file;
+    const { id } = req.params;
+
+    if(file){
+        const storage = getStorage();
+        const reff = ref(storage, `budgets/${id}/${file.originalname}`);
+        // const img = await fetch(file.buffer);
+        // const bytes = await img.blob();
+        await uploadBytes(reff, file.buffer);
+
+    }
+    res.send('hello')
+}))
 
 // @desc    Fetch deductions by the budget id
 // @route   GET /api/v1/deductions/:id
@@ -37,7 +63,7 @@ route.get('/:id', protect, asyncHandler(async (req: Request, res:Response, next:
 route.post('/:id', protect, asyncHandler(async (req: Request, res:Response, next: NextFunction) => {
     const {id} = req.params;
     const { username } = (req as any).user;
-    const { amount, description, image, tags, created_on } = req.body;
+    let { amount, description, image, tags, created_on } = req.body;
 
     if(!amount) throw new Error('Provide an amount deducted')
 
@@ -48,7 +74,12 @@ route.post('/:id', protect, asyncHandler(async (req: Request, res:Response, next
 
     const {rows: [newDeduction]} = await pool.query(`insert into "Deduction" (amount, description, image, tags, id, budgets_id, created_on)
                                                         values ($1, $2, $3, $4, $5, $6, $7) returning *`, [amount, description, image, tags, newID, id, new Date(created_on).toISOString()])
-    res.json(newDeduction)
+    
+    if(image){
+        const storage = getStorage();
+        image = await getDownloadURL(ref(storage, `budgets/${id}/${image}`))
+    }
+    res.json({...newDeduction, image })
 }));
 
 
@@ -71,28 +102,6 @@ route.post('/:budgetID/:id', protect, asyncHandler(async (req: Request, res:Resp
     res.json(deduction);
 }));
 
-route.get('/sss/sss/ss', asyncHandler(async(req: Request, res:Response, next: NextFunction) => {
-    const storage = getStorage();
-    const listRef = ref(storage, 'budgets');
-
-    // Find all the prefixes and items.
-    listAll(listRef)
-        .then((res) => {
-            res.items.forEach((itemRef) => {
-                getDownloadURL(ref(storage, `budgets/${itemRef.name}`))
-                    .then((url) => {
-                        console.log(url)
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });
-            });
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-    res.send({yes: 'yes'})
-}))
 
 // @desc    deleting a deductions by the budget-id and deduction-id
 // @route   GET /api/v1/deductions/:budgetID/:id
@@ -104,7 +113,20 @@ route.delete('/:budgetID/:id', protect, asyncHandler(async (req: Request, res:Re
     const { rows: [record] } = await pool.query(`select * from "BudgetUser" bu where bu.budget_id = $1 and username = $2`, [budgetID, username]);
     if(!record) throw new Error('You do not have permission to do that');
 
+    const {rows: [singleDeduction]} = await pool.query(`select * from "Deduction" where id = $1`, [id]);
+
     await pool.query(`delete from "Deduction" where id = $1`, [id]);
+
+    if(singleDeduction.image){
+        const storage = getStorage();
+        const desertRef = ref(storage, `budgets/${budgetID}/${singleDeduction.image}`);
+        try {
+            await deleteObject(desertRef);
+        } 
+        catch (error) { 
+            console.log(error)
+        }
+    }
 
     res.json({success: true});
 }));
